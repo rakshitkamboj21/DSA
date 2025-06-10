cb4a7dc449825869b545a5449bd901c871b5828f
rakshitkamboj21 <rakshitkamboj21@gmail.com>
2025-06-10 21:28:52 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{ 
+    int n = 5;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+        int spaces = 4 * (n - i);
+        for(int j = 1; j <= spaces; j++)
+        {
+            cout << " ";
+        }
+
+        
+        for (int j = 1; j <= i; j++)
+        {
+            cout << "* ";
+        }
+
+        
+        cout << endl;
+    }
+
+    return 0;
+}
